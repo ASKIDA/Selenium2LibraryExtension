17aaedabbb3fbba94534f6b7b8fbe2f19f030a19
overville <olivier.verville@axon-id.com>
2016-07-21 16:29:04 -0400

    converting into pip package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name = 'Selenium2LibraryExtension',
-	packages = ['Selenium2LibraryExtension'],
+	packages = ['Selenium2LibraryExtension', 'Selenium2LibraryExtension.keywords', 'Selenium2LibraryExtension.patches'],
 	version = '1.0.0',
 	description = 'This library was created as an expansion to Selenium2Library. It was initially created as a solution to specific test cases that required overhead (such as calling inline javascripts or using many steps for one action). It also features bugfixes.',
 	author = 'Olivier Verville',
@@ -11,5 +11,7 @@ setup(
 	download_url = 'https://github.com/axon-id/Selenium2LibraryExtension/tarball/1.0.0',
 	keywords = ['Robot Framework', 'Robot-Framework', 'Selenium', 'WebDriver', 'Selenium2Library'],
 	license = 'Apache License 2.0',
-	classifiers = ['Framework :: Robot Framework']
+	classifiers = ['Framework :: Robot Framework'],
+	install_requires = ['robotframework-selenium2library'],
+	
 )
