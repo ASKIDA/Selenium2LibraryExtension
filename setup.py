6e30b34a7d71050f689b4498101c32fd85a70063
overville <olivier.verville@axon-id.com>
2016-07-21 16:35:48 -0400

    pypi package

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from distutils.core import setup
 
 setup(
 	name = 'Selenium2LibraryExtension',
+	package_dir  = {'' : 'src'},
 	packages = ['Selenium2LibraryExtension', 'Selenium2LibraryExtension.keywords', 'Selenium2LibraryExtension.patches'],
-	version = '1.0.0',
+	version = '1.0.1',
 	description = 'This library was created as an expansion to Selenium2Library. It was initially created as a solution to specific test cases that required overhead (such as calling inline javascripts or using many steps for one action). It also features bugfixes.',
-	author = 'Olivier Verville',
+	author = 'Axon-id',
 	author_email = 'olivier.verville@axon-id.com',
 	url = 'https://github.com/axon-id/Selenium2LibraryExtension',
 	download_url = 'https://github.com/axon-id/Selenium2LibraryExtension/tarball/1.0.0',
 	keywords = ['Robot Framework', 'Robot-Framework', 'Selenium', 'WebDriver', 'Selenium2Library'],
 	license = 'Apache License 2.0',
 	classifiers = ['Framework :: Robot Framework'],
-	install_requires = ['robotframework-selenium2library'],
-	
+	install_requires = ['robotframework-selenium2library', 'robotframework >= 2.6.0', 'selenium >= 2.32.0'],
 )
